0dad144070847db8ffce0e28dfe3fbcd3ca74bfb
David Parker <davparker@gmail.com>
2014-07-27 20:58:54 -0500

    Workinng oon collecting info for codebook & adding fields

---
+++ get_dataframe_attr.R
@@ -0,0 +1,11 @@
+sink("strxMergData.txt", append=FALSE, split=FALSE)
+str(xMergeData, list.len = 999)
+sink()
+sink("strxMeanStdData.txt", append=FALSE, split=FALSE)
+str(xMeanStdData, list.len = 999)
+sink()
+sink("strTidyDataSet.txt", append=FALSE, split=FALSE)
+str(TidyDataSet, list.len = 999)
+sink()
+
+
